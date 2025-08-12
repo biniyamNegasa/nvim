5b2e63253d413ed604f696520e650de896f8668e
biniyamNegasa <104790161+biniyamNegasa@users.noreply.github.com>
2025-08-12 15:30:12 +0300

    fix: change theme when omarchy theme is changed

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,6 +1,18 @@
-return {
+local uv = vim.uv
+local file_path = vim.fn.expand("~/.config/omarchy/current/theme/neovim.lua")
+
+local default = {
   "LazyVim/LazyVim",
   opts = {
     colorscheme = "gruvbox",
   },
 }
+
+if uv.fs_stat(file_path) then
+  local success, result = pcall(dofile, file_path)
+  if success and type(result) == "table" then
+    return result
+  end
+end
+
+return default
